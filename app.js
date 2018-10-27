161da127e369d5293e688cfe798531b5b0ffbfa0
ANTHONY TENNENBAUM <anthony.tennenbaum@yahoo.com>
2018-10-27 10:28:47 -0500

    finished radio button validation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var error = document.getElementById("error");
 var male = document.getElementById("male");
 var femaile = document.getElementById("female");
+var gender = document.querySelector('input[name="gender"]:checked');
 
 function checkForm(frm){  
     error.innerHTML = ""; 
@@ -9,25 +10,36 @@ function checkForm(frm){
         isValid = false;
         frm.name.focus();
         frm.name.style.background = "#4286f4"  
-        error.innerHTML += "Required fields are missing";      
+        error.innerHTML += "Required fields are missing " + gender;
         error.style.display = "block";        
     }
     if(frm.email.value == ""){
         isValid = false;
         frm.email.focus();
         frm.email.style.background = "#4286f4"
-        //alert("e");
         error.style.display = "block";          
+    }
+    if(( frm.gender[0].checked == false ) && ( frm.gender[1].checked == false ) )
+    {
+        error.innerHTML += "Must pick a gender " + "\n";
+        error.style.display = "block";
+        isValid = false;
     }
     if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(frm.email.value)){       
         
        
     }else{
-        error.innerHTML += "invalid email";
+        error.innerHTML += "invalid email " + "\n";
         error.style.display = "block"; 
         isValid = false;
     }
+
     
+        
     //validateEmail(frm)  
     return isValid;
+}
+
+function atLeastOneRadio(){    
+    return ($('input[type=radio]:checked').size() > 0);
 }
