04b54c7f29b885dee8a39ccf43da92d041b2f1f0
ANTHONY TENNENBAUM <anthony.tennenbaum@yahoo.com>
2018-10-29 10:51:24 -0500

    Added in the GET method on the form

--- app.js
+++ app.js
@@ -10,43 +10,46 @@ function checkForm(frm){                                    // Checks for invali
     if(frm.name.value == "" || frm.name.value.length < 6){  // Checks to see if name is less than 6 characters, throws error to user
         isValid = false;
         frm.name.focus();
-        frm.name.style.background = "#4286f4"  
-        error.innerHTML += "Required fields are missing " + gender;
+        frm.name.style.background = "#f44b42";  
+        error.innerHTML += "Required fields are missing. " + gender;
         error.style.display = "block";        
+    }else{
+        frm.name.style.background = "";  
     }
     if(frm.email.value == ""){                             // Checks to see if email field is blank
         isValid = false;
         frm.email.focus();
-        frm.email.style.background = "#4286f4"
+        frm.email.style.background = "#f44b42";
+        error.innerHTML = "Must enter a email. ";
         error.style.display = "block";          
+    }else{
+        frm.email.style.background = "";  
     }
     if(( frm.gender[0].checked == false ) && ( frm.gender[1].checked == false ) )  // Checks to see if both radios are unchecked
     {
-        error.innerHTML += "Must pick a gender " + "\n";
+        error.innerHTML += "Must pick a gender. ";
         error.style.display = "block";
         isValid = false;
     }
     if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(frm.email.value)){   // Verifies that email is a valid email.    
-        
-       
+        frm.email.style.background = "";       
     }else{
-        error.innerHTML += "invalid email " + "\n";
+        frm.email.style.background = "#f44b42";
+        error.innerHTML += "Invalid email. ";
         error.style.display = "block"; 
         isValid = false;
     }
     if(/^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/.test(frm.phone.value))  // Verifies that phone number is valid as US Phone number
     {
-      
+        frm.phone.style.background = "";
     }else
-    {   error.innerHTML += "You have entered an invalid Phone Number" + "\n";
+    {   
+        frm.phone.style.background = "#f44b42";
+        error.innerHTML += "You have entered an invalid Phone Number. ";
         error.style.display = "block";
         isValid = false;
 
     }
-
-    
-        
-    //validateEmail(frm)  
     return isValid;
 }
 
